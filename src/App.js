e72b7882ae2d7efd862661dc3f6184328e0ca16c
Tyler Wray <wraytw@gmail.com>
2018-01-08 10:08:48 -0700

    Adding basic modifications for design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -7,11 +6,16 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">
+            <span className="App-first-name">Tyler</span>
+            &nbsp;
+            <span className="App-last-name">Wray</span>
+          </h1>
         </header>
+        <div className="App-portrait"></div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.
         </p>
       </div>
     );

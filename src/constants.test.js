a0a83a4a80f4cc1fd313f7547c46efeb9e0f7054
Tyler Wray <wraytw@gmail.com>
2018-01-14 16:25:19 -0700

    Editing some configuration

---
+++ src/constants.test.js
@@ -0,0 +1,11 @@
+import * as constants from './constants';
+
+describe('Constants', () => {
+  it('should export OPEN_SIDE_NAV', () => {
+    expect(constants.OPEN_SIDE_NAV).toEqual('OPEN_SIDE_NAV');
+  });
+
+  it('should export CLOSE_SIDE_NAV', () => {
+    expect(constants.CLOSE_SIDE_NAV).toEqual('CLOSE_SIDE_NAV');
+  });
+});

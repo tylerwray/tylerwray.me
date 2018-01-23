df1351b193e8f91e35ff5700bbae92d3b68459e6
Tyler Wray <wraytw@gmail.com>
2018-01-23 14:48:37 -0700

    Adding a sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ import NavBar from './NavBar';
 
 import '../styles/Header.css';
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = () => {
+  const navbar = document.querySelector('.Header');
+
+  if (window.pageYOffset > navbar.offsetTop) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+};
+
 const Header = () => (
   <header className="Header">
     <div className="Header-background"></div>

cc2f59330e12e0f75d7cbaf3adcb3aa67871e2e1
Tyler Wray <wraytw@gmail.com>
2018-01-25 13:07:34 -0700

    Removing sticky header for now.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,17 @@ import NavBar from './NavBar';
 
 import '../styles/Header.css';
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = () => {
-  const navbar = document.querySelector('.Header');
+// TODO: Make the scroll more fluid on mobile prob a css thing
+// When the user scrolls the page make the header sticky
+// window.onscroll = () => {
+//   const navbar = document.querySelector('.Header');
 
-  if (window.pageYOffset > navbar.offsetTop) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-};
+//   if (window.pageYOffset > navbar.offsetTop) {
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');
+//   }
+// };
 
 const Header = () => (
   <header className="Header">

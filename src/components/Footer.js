bdb13f159de3116a9e6f9cbebf80036fc893d64d
Tyler Wray <wraytw@gmail.com>
2018-01-26 17:25:00 -0700

    Adding Contact icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,19 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 
+import Email from '../images/email.svg.js';
+import Github from '../images/github.svg.js';
+import Twitter from '../images/twitter.svg.js';
+import Facebook from '../images/facebook.svg.js';
+import Linkedin from '../images/linkedin.svg.js';
+
 import '../styles/Footer.css';
 
-const label = {
-  fontWeight: '100',
-  width: '80px',
-  display: 'inline-block'
+const iconStyle = {
+  height: '30px',
+  fill: '#FFFFFF',
+  marginRight: '20px',
+  marginBottom: '20px'
 };
 
 const Footer = () => (
@@ -17,12 +24,23 @@ const Footer = () => (
     <Grid>
       <Row style={{ maxWidth: '700px' }}>
         <Col xs={12} sm={6} md={4}>
-          <h4 style={{ paddingLeft: '20px' }}>Contact</h4>
-          <div><div style={label}>Email</div><a href="mailto:wraytw@gmail.com">wraytw@gmail.com</a></div>
-          <div><div style={label}>Github</div><a href="https://github.com/wraytw" rel="noopener noreferrer" target="_blank">wraytw</a></div>
-          <div><div style={label}>Twitter</div><a href="https://twitter.com/wray_tw" rel="noopener noreferrer" target="_blank">@wray_tw</a></div>
-          <div><div style={label}>Facebook</div><a href="https://www.facebook.com/wraytw" rel="noopener noreferrer" target="_blank">Tyler James Wray</a></div>
-          <div><div style={label}>LinkedIn</div><a href="https://www.linkedin.com/in/wraytw/" rel="noopener noreferrer" target="_blank">Tyler James Wray</a></div>
+          <h4>Contact</h4>
+          <a href="mailto:wraytw@gmail.com">
+            <Email style={iconStyle} />
+          </a>
+          <a href="https://github.com/wraytw" rel="noopener noreferrer" target="_blank">
+            <Github style={iconStyle} />
+          </a>
+          <div></div>
+          <a href="https://twitter.com/wray_tw" rel="noopener noreferrer" target="_blank">
+            <Twitter style={iconStyle} />
+          </a>
+          <a href="https://www.facebook.com/wraytw" rel="noopener noreferrer" target="_blank">
+            <Facebook style={iconStyle}/>
+          </a>
+          <a href="https://www.linkedin.com/in/wraytw/" rel="noopener noreferrer" target="_blank">
+            <Linkedin style={iconStyle}/>
+          </a>
         </Col>
       </Row>
       <div style={{ marginTop: '30px' }}></div>

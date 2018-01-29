6d2e75172feb2fc051916462558f77a2a5a2ac38
Tyler Wray <wraytw@gmail.com>
2018-01-29 00:01:30 -0700

    Fixing tests. I thought I was a cowboy coder for a min

--- src/components/Footer.test.js
+++ src/components/Footer.test.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Footer from './Footer';
 
+jest.mock('material-ui/FlatButton', () => () => 'div');
+
 it('should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Footer />, div);

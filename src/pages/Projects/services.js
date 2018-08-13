f3fde7ffe57d26e4791189e26b4bd313514be7d0
Tyler Wray <wraytw@gmail.com>
2018-08-13 06:08:17 -0600

    Prettifying all the files

--- src/pages/Projects/services.js
+++ src/pages/Projects/services.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 export function getRepos() {
-  return axios
-    .get('https://api.github.com/users/tylerwray/repos')
-    .then(res => res.data.map(repo => ({
+  return axios.get('https://api.github.com/users/tylerwray/repos').then(res =>
+    res.data.map(repo => ({
       name: repo.name,
       description: repo.description,
       url: repo.html_url,
       language: repo.language,
       updated_at: repo.updated_at
-    })))
+    }))
+  )
 }
--- src/saga.js
+++ src/saga.js
@@ -1,8 +1,6 @@
 import { all } from 'redux-saga/effects'
 import projectsSaga from './pages/Projects/saga'
 
-export default function * rootSaga() {
-  yield all([
-    projectsSaga()
-  ])
+export default function* rootSaga() {
+  yield all([projectsSaga()])
 }

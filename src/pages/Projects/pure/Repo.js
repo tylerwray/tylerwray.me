45506fa35bb9297314396e790196aabc3fdd17a7
Tyler Wray <wraytw@gmail.com>
2018-08-19 23:42:51 -0600

    Splitting up components

---
+++ src/pages/Projects/pure/Repo.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+
+import Github from '../media/images/github.svg'
+
+function styles(theme) {
+  const card = {
+    minWidth: 275,
+    maxWidth: 275,
+    margin: 10
+  }
+
+  return {
+    card,
+    title: {
+      marginBottom: 16
+    },
+    description: {
+      height: 50
+    },
+    gitHubIcon: {
+      fill: theme.palette.secondary.main,
+      height: 20,
+      marginRight: 10
+    },
+    gitHubText: {
+      color: theme.palette.secondary.main
+    }
+  }
+}
+
+function Repo(props) {
+  const { classes, repo } = props
+
+  return (
+    <Grid item key={repo.url}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography className={classes.title} variant="title">
+            {repo.name}
+          </Typography>
+          <Typography className={classes.description} color="textSecondary">
+            {repo.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button href={repo.html_url} color="secondary" target="_blank">
+            <Github className={classes.gitHubIcon} />
+            <span className={classes.gitHubText}>View on Github</span>
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  )
+}
+
+Repo.propTypes = {
+  classes: PropTypes.object.isRequired,
+  repo: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Repo)
--- src/pages/Projects/pure/Repos.js
+++ src/pages/Projects/pure/Repos.js
@@ -2,71 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { withStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 
-import Github from '../media/images/github.svg'
+import Repo from './Repo'
 
 function styles(theme) {
-  const card = {
-    minWidth: 275,
-    maxWidth: 275,
-    margin: 10
-  }
-
   return {
     root: {
       marginTop: theme.spacing.unit * 2
-    },
-    card,
-    loaderCard: {
-      ...card,
-      height: 175
-    },
-    title: {
-      marginBottom: 16
-    },
-    description: {
-      height: 50
-    },
-    gitHubIcon: {
-      fill: theme.palette.secondary.main,
-      height: 20,
-      marginRight: 10
-    },
-    gitHubText: {
-      color: theme.palette.secondary.main
     }
   }
 }
+
 function Repos(props) {
   const { classes, repos } = props
 
   return (
     <Grid className={classes.root} container justify="space-around">
       {repos.map(repo => (
-        <Grid item key={repo.url}>
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography className={classes.title} variant="title">
-                {repo.name}
-              </Typography>
-              <Typography className={classes.description} color="textSecondary">
-                {repo.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button href={repo.html_url} color="secondary" target="_blank">
-                <Github className={classes.gitHubIcon} />
-                <span className={classes.gitHubText}>View on Github</span>
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Repo key={repo.id} repo={repo} />
       ))}
     </Grid>
   )

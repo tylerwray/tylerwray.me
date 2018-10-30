9cf2ca654b449c149b84f6cd9048bd878216a160
Tyler Wray <wraytw@gmail.com>
2018-10-30 14:09:08 -0600

    Adding google analytics

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react'
+import ReactGA from 'react-ga'
 
-import { Router } from '@reach/router'
+import { Location, Router } from '@reach/router'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 import HomePage from './components/HomePage'
@@ -9,15 +10,24 @@ const ProjectsPage = React.lazy(() => import('./components/ProjectsPage'))
 const ContactPage = React.lazy(() => import('./components/ContactPage'))
 const WorkHistoryPage = React.lazy(() => import('./components/WorkHistoryPage'))
 
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_UA_CODE)
+
 function Routes() {
   return (
     <Suspense fallback={<CircularProgress />}>
-      <Router>
-        <HomePage path="/" />
-        <ProjectsPage path="/projects" />
-        <ContactPage path="/contact" />
-        <WorkHistoryPage path="/work-history" />
-      </Router>
+      <Location>
+        {({ location }) => {
+          ReactGA.pageview(location.href)
+          return (
+            <Router>
+              <HomePage path="/" />
+              <ProjectsPage path="/projects" />
+              <ContactPage path="/contact" />
+              <WorkHistoryPage path="/work-history" />
+            </Router>
+          )
+        }}
+      </Location>
     </Suspense>
   )
 }
